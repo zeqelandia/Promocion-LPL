e1e9f91dcc6820fe470d7fc7f5695b1b5658b742
zeqelandia <enzzio2@hotmail.com>
2018-07-13 20:02:52 -0300

    Log In y Crear Usuario
    Se completo el desarrollo del apartado visual de las pantallas de log in y de crear usuario
    
    Co-Authored-By: Ezequiel Silvestre Diaz <zeqelandia@users.noreply.github.com>

--- JS/index.js
+++ JS/index.js
@@ -1,18 +1,71 @@
 function ingresar(){
 	var ventanaIngreso = document.getElementById("contenedor_ingresar");
+	var ventanaLogIn = document.getElementById("contenedor_login");
+	var ventanaCrearCuenta = document.getElementById("contenedor_crearCuenta");
+
 	ventanaIngreso.classList.remove("fadeOutUp");
 	ventanaIngreso.style.visibility= "visible";
+	ventanaLogIn.style.visibility= "visible";
 	ventanaIngreso.classList.add("fadeInDown");
 }
 
 function cerrarIngreso(){
 	var ventanaIngreso = document.getElementById("contenedor_ingresar");
+	var ventanaLogIn = document.getElementById("contenedor_login");
+	var ventanaCrearCuenta = document.getElementById("contenedor_crearCuenta");
 	
 	ventanaIngreso.classList.remove("fadeInDown");
 	ventanaIngreso.classList.add("fadeOutUp");
 	//ventanaIngreso.style.visibility= "hidden";
 
 	setTimeout(function(){
+		ventanaLogIn.style.visibility= "hidden";
 		ventanaIngreso.style.visibility= "hidden";
 	}, 1000);
+}
+
+function menuCrearCuenta(){
+	var ventanaIngreso = document.getElementById("contenedor_ingresar");
+	var ventanaLogIn = document.getElementById("contenedor_login");
+	var ventanaCrearCuenta = document.getElementById("contenedor_crearCuenta");
+
+	ventanaCrearCuenta.classList.remove("fadeOut");
+	ventanaLogIn.classList.add("fadeOut");
+
+	setTimeout(function(){
+		ventanaLogIn.style.visibility= "hidden";
+	}, 1000);
+
+
+	ventanaCrearCuenta.style.visibility= "visible";
+	ventanaCrearCuenta.classList.add("fadeIn");
+}
+
+function volver(){
+	var ventanaIngreso = document.getElementById("contenedor_ingresar");
+	var ventanaLogIn = document.getElementById("contenedor_login");
+	var ventanaCrearCuenta = document.getElementById("contenedor_crearCuenta");
+
+	ventanaLogIn.classList.remove("fadeOut");
+	ventanaCrearCuenta.classList.add("fadeOut");
+
+	setTimeout(function(){
+		ventanaCrearCuenta.style.visibility= "hidden";
+	}, 1000);
+
+
+	ventanaLogIn.style.visibility= "visible";
+	ventanaLogIn.classList.add("fadeIn");
+}
+
+function mostrarAyuda(){
+	var ventanaAyuda = document.getElementById("contenedor_ayuda");
+
+	ventanaAyuda.style.visibility= "visible";
+}
+
+function esconderAyuda(){
+	var ventanaAyuda = document.getElementById("contenedor_ayuda");
+
+	ventanaAyuda.style.visibility= "hidden";
 }
